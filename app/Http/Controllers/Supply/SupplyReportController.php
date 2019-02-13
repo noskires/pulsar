94b62e5725423c517354c6dba1b06c3bbd9c6bfd
noskires <ebsupnet@gmail.com>
2019-02-13 23:57:36 +0800

    fixed: stock card reports

--- app/Http/Controllers/Supply/SupplyReportController.php
+++ app/Http/Controllers/Supply/SupplyReportController.php
@@ -36,27 +36,27 @@ class SupplyReportController extends Controller {
 
 		$data['stock_items'] = $receipt_items->union($requisition_slips_items)->orderBy('date')->get();
 
-		foreach ($data['stock_items'] as $key => $stock_item) {
-			if($stock_item->payee_type=="SUPPLIER")
-			{
-				$supplier = DB::table('suppliers as s')
-						->select('s.supplier_name')
-						->where('s.supplier_code', $stock_item->payee)->first();
+		// foreach ($data['stock_items'] as $key => $stock_item) {
+		// 	if($stock_item->payee_type=="SUPPLIER")
+		// 	{
+		// 		$supplier = DB::table('suppliers as s')
+		// 				->select('s.supplier_name')
+		// 				->where('s.supplier_code', $stock_item->payee)->first();
 
-				if($supplier)
-				{
-					$stock_item->particulars = $supplier->supplier_name . " ( ".$stock_item->payee." )";
-				}
-				else
-				{
-					$stock_item->particulars = $stock_item->asset_name;
-				}
-			}
-			else
-			{
-				$stock_item->particulars = $stock_item->asset_name;
-			}
-		}
+		// 		if($supplier)
+		// 		{
+		// 			$stock_item->particulars = $supplier->supplier_name . " ( ".$stock_item->payee." )";
+		// 		}
+		// 		else
+		// 		{
+		// 			$stock_item->particulars = $stock_item->asset_name;
+		// 		}
+		// 	}
+		// 	else
+		// 	{
+		// 		$stock_item->particulars = $stock_item->asset_name;
+		// 	}
+		// }
 
 		// return $data['stock_items'];
 
@@ -81,9 +81,12 @@ class SupplyReportController extends Controller {
 					DB::raw('"RCP" as type'),
 					'r.payee_type as payee_type',
 					'r.payee as payee',
-					DB::raw('null as asset_name')
+					DB::raw('null as request_type'),
+					'supplier.supplier_name as particulars'
+					
 				)
 				->leftjoin('receipts as r','r.receipt_code','=','ri.receipt_code')
+				->leftjoin('suppliers as supplier','supplier.supplier_code','=','r.payee')
 				->where('ri.receipt_item_supply_code', $supplyCode);
 		return $data;
 	}
@@ -98,11 +101,28 @@ class SupplyReportController extends Controller {
 					DB::raw('"RIS" as type'),
 					DB::raw('null as payee_type'),
 					DB::raw('null as payee'),
-					DB::raw('CONCAT(CONCAT(a.name," ( ",COALESCE(a.code,"")," )")) as asset_name')
+					// DB::raw('CONCAT(CONCAT(a.name," ( ",COALESCE(a.code,"")," )")) as asset_name'),
+
+					DB::raw('CASE 
+						WHEN rs.request_type = "Office" 
+							THEN "Office"
+						WHEN rs.request_type = "Project"
+							THEN "Project"
+						ELSE "" 
+						END AS request_type'
+					),
+
+					DB::raw('CASE 
+						WHEN rs.request_type = "Office" 
+							THEN (SELECT CONCAT(org_name," (",org_code,")")  FROM organizations WHERE org_code=rs.reference_code)
+						WHEN rs.request_type = "Project"
+							THEN (SELECT CONCAT(name," (",code,")")  FROM projects WHERE project_code=rs.reference_code)
+						ELSE "" 
+						END AS particulars'
+					)
 				)
 				->join('requisition_slips_items as ris','ris.requisition_slip_code','=','rs.requisition_slip_code')
-				->leftjoin('job_orders as jo','jo.job_order_code','=','rs.reference_code')
-				->leftjoin('assets as a','a.tag','=','jo.asset_tag')
+				// ->leftjoin('assets as a','a.tag','=','jo.asset_tag')
 				->where('ris.supply_code', $supplyCode);
 		return $data;
 	}
