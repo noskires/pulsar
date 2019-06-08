71bd1965354cafa8d04ac50e3d832b9eec230889
Rommel Penaflor <penaflor024@gmail.com>
2019-06-08 17:45:38 +0800

    Change photo size

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -3,7 +3,7 @@
   <!-- Sidebar user panel -->
   <div class="user-panel">
     <div class="pull-left image">
-      <img src="<%mc.users[0].profile_photo || 'uploads/profile_photo/default-logo.jpg'%>" class="img-circle" alt="User Image" style="width: 45px;height: 45px;">
+      <img src="<%mc.users[0].profile_photo || 'uploads/profile_photo/default-logo.jpg'%>" class="img-circle" alt="User Image">
     </div>
     <div class="pull-left info">
       <p>{{Auth::user()->name}}</p>
