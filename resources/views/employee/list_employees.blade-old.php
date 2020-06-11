cc4f68ed0add27e7bdd1bc76a27f3d492193d444
GenErik <ebsupnet@gmail.com>
2020-06-11 17:58:43 +0800

    add suffix employee module

--- resources/views/employee/list_employees.blade-old.php
+++ resources/views/employee/list_employees.blade-old.php
@@ -5,7 +5,7 @@
   <section class="content-header">
   <h1><span class="fa fa-user"> </span> Manage Employees</h1>
   <ol class="breadcrumb">
-    <li><a href=""><i class="fa fa-th"></i> Dashboard</a></li>
+    <li><a href="../../index.html"><i class="fa fa-th"></i> Dashboard</a></li>
     <li class="active">Employee Manager</li>
   </ol>
   </section>
@@ -14,17 +14,17 @@
     
   <section class="content" id="load_div">
     <div class="row">
-      <div id="button-top" class="col-md-12"> 
+      <div id="button-top" class="col-md-9"> 
   <!-- BUTTONS -->
         <button class="btn btn-primary" data-toggle="collapse" data-target="#create-voucher" data-parent="#btn-top">
             <span class="glyphicon glyphicon-plus"></span> New Employee
-        </button> 
+        </button> &nbsp;&nbsp;&nbsp;
+        <button class="btn btn-primary" data-toggle="collapse" data-target="#filter" data-parent="#btn-top">
+            <span class="glyphicon glyphicon-refresh"></span> Filter Display
+        </button> &nbsp;&nbsp;&nbsp;
         <button type="button" class="btn btn-primary" ng-click="ec.addNewPosition()"> 
           <span class="glyphicon glyphicon-plus"></span> New Position
-        </button> 
-        <!-- <button class="btn btn-primary" data-toggle="collapse" data-target="#filter" data-parent="#btn-top">
-            <span class="glyphicon glyphicon-refresh"></span> Filter Display
-        </button> -->
+        </button>
           <br>
 
   <!-- NEW EMPLOYEE -->
@@ -40,15 +40,9 @@
             <div class="box-body">
               <div class="form-group col-sm-12">
                 <label for="controlnumber" class="col-sm-2 control-label">Employee ID</label>
-                <div class="col-sm-4">
-                  <input type="number" class="form-control" ng-model="ec.employeeDetails.employeeID" tabindex="1">
-                </div>
-
-                <label for="controlnumber" class="col-sm-2 control-label">Suffix</label>
-                <div class="col-sm-1"><input type="text" class="form-control" tabindex="5" ng-model="ec.employeeDetails.suffix"></div>
-
-                <label for="dv-payee-type" class="col-sm-1 control-label">Gender</label>
-                <div class="radio col-sm-2">
+                <div class="col-sm-3"><input type="number" class="form-control" ng-model="ec.employeeDetails.employeeID"></div>
+                <label for="dv-payee-type" class="col-sm-3 control-label">Gender</label>
+                <div class="radio col-sm-3">
                   <label><input type="radio" ng-model="ec.employeeDetails.gender" value="Male">Male</label>
                   &nbsp;&nbsp;&nbsp;
                   <label><input type="radio" ng-model="ec.employeeDetails.gender" value="Female">Female</label>
@@ -57,33 +51,30 @@
 
               <div class="form-group col-sm-12">
                 <label for="controlnumber" class="col-sm-2 control-label">Last Name</label>
-                <div class="col-sm-4">
-                  <input type="text" class="form-control" ng-model="ec.employeeDetails.lname" required="" tabindex="2">
-                </div>
-
+                <div class="col-sm-4"><input type="text" class="form-control" ng-model="ec.employeeDetails.lname" required=""></div>
                 <label for="controlnumber" class="col-sm-2 control-label">Birthdate</label>
-                <div class="col-sm-4">
+                <div class="col-sm-2">
                   <div class="input-group date">
                     <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
-                    <input type="text" class="form-control pull-right" id="datepicker-bday" ng-model="ec.employeeDetails.bday" datepicker autocomplete="off" tabindex="6">
+                    <input type="text" class="form-control pull-right" id="datepicker-bday" ng-model="ec.employeeDetails.bday" datepicker autocomplete="off">
                   </div>
                 </div>
+                <label for="controlnumber" class="col-sm-1 control-label">Suffix</label>
+                <div class="col-sm-1"><input type="text" class="form-control"></div>
               </div>
 
               <div class="form-group col-sm-12">
                 <label for="controlnumber" class="col-sm-2 control-label">First Name</label>
-                <div class="col-sm-4"><input type="text" class="form-control" ng-model="ec.employeeDetails.fname" required="" tabindex="3"></div>
-
+                <div class="col-sm-4"><input type="text" class="form-control" ng-model="ec.employeeDetails.fname" required=""></div>
                 <label for="controlnumber" class="col-sm-2 control-label">Phone Number</label>
-                <div class="col-sm-4"><input type="number" class="form-control" ng-model="ec.employeeDetails.phone_no" tabindex="7"></div>
+                <div class="col-sm-3"><input type="number" class="form-control" ng-model="ec.employeeDetails.phone_no"></div>
               </div>
 
               <div class="form-group col-sm-12">
                 <label for="controlnumber" class="col-sm-2 control-label">Middle Name</label>
-                <div class="col-sm-4"><input type="text" class="form-control" ng-model="ec.employeeDetails.mname" tabindex="4"></div>
-
+                <div class="col-sm-4"><input type="text" class="form-control" ng-model="ec.employeeDetails.mname"></div>
                 <label for="controlnumber" class="col-sm-2 control-label">Email Address</label>
-                <div class="col-sm-4"><input type="text" class="form-control" ng-model="ec.employeeDetails.email" tabindex="8"></div>
+                <div class="col-sm-3"><input type="text" class="form-control" ng-model="ec.employeeDetails.email"></div>
               </div>
 
               <div class="form-group col-sm-12">
@@ -96,14 +87,14 @@
                 <label class="col-sm-2 control-label">Job Title</label>
                 <div class="col-sm-4">
                   <select class="form-control select2" style="width: 100%;" required="" ng-model="ec.employeeDetails.position_code">
-                    <option selected="selected" value="">SELECT JOB TITLE</option>
+                    <option selected="selected" value="">- - SELECT JOB TITLE- -</option>
                     <option ng-value="position.position_code" ng-repeat="position in ec.positions"><%position.position_text%></option>
                   </select>
                 </div>
                 <label class="col-sm-2 control-label">Department</label>
                 <div class="col-sm-4">
                   <select class="form-control select2" style="width: 100%;" required="" ng-model="ec.employeeDetails.department" ng-change="ec.selectDepartment(ec.employeeDetails.department)">
-                    <option selected="selected" value="">SELECT DEPARTMENT</option>
+                    <option selected="selected" value="">- - SELECT DEPARTMENT - -</option>
                     <option ng-value="department.org_code" ng-repeat="department in ec.departments"><%department.department_name%></option>
                   </select>
                 </div>
@@ -113,24 +104,29 @@
                 <label class="col-sm-2 control-label">Division</label>
                 <div class="col-sm-4">
                   <select class="form-control select2" style="width: 100%;" required="" ng-model="ec.employeeDetails.division" ng-change="ec.selectDivision(ec.employeeDetails.division)">
-                    <option selected="selected" value="">SELECT DIVISION</option>
+                    <option selected="selected" value="">- - SELECT DIVISION - -</option>
                     <option ng-value="division.org_code" ng-repeat="division in ec.divisions"><%division.division_name%></option>
                   </select>
                 </div>
                 <label class="col-sm-2 control-label">Unit</label>
                 <div class="col-sm-4">
                     <select class="form-control select2" style="width: 100%;" required="" ng-model="ec.employeeDetails.unit">
-                      <option selected="selected" value="">SELECT UNIT</option>
+                      <option selected="selected" value="">- - SELECT UNIT - -</option>
                       <option ng-value="unit.org_code" ng-repeat="unit in ec.units"><%unit.unit_name%></option>
                     </select>
                 </div>
               </div>
-              
+                          
+              <div class="form-group col-sm-12">
+                <hr style="border-color:#e1e1e1;border-width:1px 0;">
+                <!-- <label class="col-sm-2 control-label"></label>
+                <div class="col-sm-9 text-blue"><h4><i class="fa fa-address-book-o"></i><b> Address</b></h4></div> -->
+              </div>
 
-              <div class="form-group col-sm-12"> <br>          
+              <div class="form-group col-sm-12">           
                 <div class="col-sm-8"></div>
                 <div class="col-sm-4">
-                <button class="btn btn-large btn-success btn-block" data-toggle="confirmation"
+                <button class="btn btn-large btn-primary btn-block" data-toggle="confirmation"
                 data-btn-ok-label="Save" data-btn-ok-icon="fa fa-check" data-btn-ok-class="btn-success"
                 data-btn-cancel-label="Cancel" data-btn-cancel-icon="fa fa-times" data-btn-cancel-class="btn-danger"
                 data-title="Confirm data entry." data-content="Are you sure?" ng-click="ec.submit(ec.employeeDetails)"> CONFIRMATION
@@ -152,7 +148,7 @@
           <div class="box-body">
           <!-- form start -->
           <form id="from-unit" class="form-horizontal" role="form">
-            <div class="form-group col-sm-12">
+            <div class="form-group col-sm-12"">
               <div class="col-sm-3">
               <select class="form-control select2" style="width: 100%;" required="">
               <option selected="selected" value="0">Select Division</option>
@@ -176,7 +172,7 @@
     <div class="box box-primary">
       <div class="box-body">
         <div export-to-xlsx data="ec.employees" bind-to-table="'tb-employee'" filename="'Employee Masterfile'"></div>
-        <table datatable="ng" class="table table-bordered table-hover" name="tb-employee" width="100%" style="font-size: 13px;">
+        <table datatable="ng" class="table table-bordered table-hover" name="tb-employee" width="100%">
           <thead>
           <tr>
             <th>Employee ID</th>
@@ -190,10 +186,10 @@
             <th>Phone No.</th>
             <th>Department</th>
             <th>Division</th>
-            <!-- <th>TIN</th> -->
-            <!-- <th>SSS</th> -->
-            <!-- <th>Philhealth</th> -->
-            <!-- <th>Pag-ibig</th> -->
+            <th>TIN</th>
+            <th>SSS</th>
+            <th>Philhealth</th>
+            <th>Pag-ibig</th>
             <!-- <th>Unit</th> -->
           </tr>
           </thead>
@@ -210,10 +206,10 @@
             <td><%employee.phone_number%></td>
             <td><%employee.department%></td>
             <td><%employee.division%></td>
-            <!-- <td><%employee.tin%></td> -->
-            <!-- <td><%employee.sss_number%></td> -->
-            <!-- <td><%employee.philhealth_number%></td> -->
-            <!-- <td><%employee.pagibig_number%></td> -->
+            <td><%employee.tin%></td>
+            <td><%employee.sss_number%></td>
+            <td><%employee.philhealth_number%></td>
+            <td><%employee.pagibig_number%></td>
             <!-- <td><%employee.unit%></td> -->
           </tr>
           </tbody>
@@ -279,7 +275,7 @@
                         <label class="col-sm-3 control-label">Gender</label>
                         <div class="col-sm-9">
                         <select class="form-control select2" style="width: 100%;" required="" ng-model="vm.formData.gender">
-                          <option selected="selected" value="">SELECT GENDER</option>
+                          <option selected="selected" value="">- - SELECT GENDER - -</option>
                           <option value="Male">Male</option>
                           <option value="Female">Female</option>
                         </select>
@@ -289,7 +285,7 @@
                         <label class="col-sm-3 control-label">Job Title</label>
                         <div class="col-sm-9">
                         <select class="form-control select2" style="width: 100%;" required="" ng-model="vm.formData.position_code">
-                          <option selected="selected" value="0">SELECT JOB TITLE</option>
+                          <option selected="selected" value="0">- - select job title - -</option>
                           <option ng-value="position.position_code" ng-repeat="position in vm.positions"><%position.position_text%></option>
                         </select></div> 
                       </div>
@@ -314,7 +310,7 @@
                         <label class="col-sm-3 control-label">Department</label>
                         <div class="col-sm-9">
                         <select class="form-control select2" style="width: 100%;" required="" ng-model="vm.formData.department_code" ng-change="vm.selectDepartment(vm.formData.department_code)">
-                          <option selected="selected" value="">SELECT DEPARTMENT</option>
+                          <option selected="selected" value="">- - SELECT DEPARTMENT - -</option>
                           <option ng-value="department.org_code" ng-repeat="department in vm.departments"><%department.department_name%></option>
                         </select></div>
                       </div>
@@ -322,7 +318,7 @@
                         <label class="col-sm-3 control-label">Division</label>
                         <div class="col-sm-9">
                         <select class="form-control select2" style="width: 100%;" required="" ng-model="vm.formData.division_code" ng-change="vm.selectDivision(vm.formData.division_code)">
-                          <option selected="selected" value="">SELECT DIVISION</option>
+                          <option selected="selected" value="">- - SELECT DIVISION - -</option>
                           <option ng-value="division.org_code" ng-repeat="division in vm.divisions"><%division.division_name%></option>
                         </select></div>
                       </div>
@@ -330,19 +326,14 @@
                         <label class="col-sm-3 control-label">Unit</label>
                         <div class="col-sm-9">
                         <select class="form-control select2" style="width: 100%;" required="" ng-model="vm.formData.unit_code">
-                          <option ng-value="">SELECT UNIT</option>
+                          <option ng-value="">- - SELECT UNIT - -</option>
                           <option ng-value="unit.org_code" ng-repeat="unit in vm.units"><%unit.unit_name%></option>
                         </select></div>
                       </div>
 
                       <div class="form-group col-sm-12">
                         <label class="col-sm-3 control-label">Date Hired</label>
-                        <div class="col-sm-9">
-                          <div class="input-group date">
-                          <div class="input-group-addon"><i class="fa fa-calendar"></i></div>
-                          <input type="text" class="form-control" id="emp_sfx" ng-model="vm.formData.date_hired" datepicker autocomplete="off">
-                          </div>
-                        </div>
+                        <div class="col-sm-9"><input type="text" class="form-control" id="emp_sfx" ng-model="vm.formData.date_hired" datepicker autocomplete="off"></div>
                       </div>
 
                       <div class="form-group col-sm-12">
@@ -454,4 +445,4 @@
     <!-- /.modal-dialog -->
   </div>
 
-  <!-- /.modal
+  <!-- /.modal -->
